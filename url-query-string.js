2835f946a3fc11134a9057323163c78dd2ec654e
DESKTOP-ST2H7RM\lenovo <ajikowiyu@gmail.com>
2023-07-12 20:44:55 +0700

    querystring via web

--- url-query-string.js
+++ url-query-string.js
@@ -5,6 +5,15 @@ const port          = 3000
 
 
 http.createServer( function(req, res) {
+    //percobaan via web
+    //buka browser, masukkan url http://localhost:3000/profil?nama=aji&pekerjaan=programmer
+    let parsequery = URL.parse(req.url).query
+    let url_querystring = querystring.parse( parsequery )
+
+    res.writeHead(200, {'Content-Type' : 'text/plain'})
+    res.write('nama saya adalah ' + url_querystring.nama + ' pekerjaan saya ' + url_querystring.pekerjaan)
+    res.end()
+
     //percobaan via console
     console.log( req.url )
     console.log( URL.parse(req.url) )
